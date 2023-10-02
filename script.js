b650f4d1a7ca38067bc2f3c05806a593aba051cd
Santiago Segre <110129047+santiagosegre@users.noreply.github.com>
2023-10-02 17:05:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// var slideIndex = 0;
+// showSlides();
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     if (slideIndex >= slides.length) {
+//         slideIndex = 0;
+//     }
+//     if (slideIndex < 0) {
+//         slideIndex = slides.length - 1;
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex].style.display = "block";
+//     setTimeout(showSlides, 5000); // Troca de imagem a cada 5 segundos
+// }
